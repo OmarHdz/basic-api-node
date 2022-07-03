0712422d294d2112eb14a1ce405e192be931868d
OmarHdz <omarhg_@hotmail.com>
2022-07-03 14:31:33 -0500

    change port to PORT error

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post("/pos", function (req, res) {
 	console.log(idm)
 })
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}`)
 })
